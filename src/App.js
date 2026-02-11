289307d2942f27fc571c266ac906246f84944a8a
IvanPrykhodko314 <i.m.prykhodko@student.itc.khai.edu>
2026-02-11 07:38:59 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Привіт, React!</h1>
+      <p>Це мій перший React-застосунок.</p>
+    </div>
+  );
+}
+
+export default App;
